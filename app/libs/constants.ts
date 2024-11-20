ae24825a459c8c71b34283bfbf02d1dff9f8ebb1
Darshan Chobarkar <dchobarkar@gmail.com>
2024-11-20 12:28:51 +0530

    add navbar

---
+++ app/libs/constants.ts
@@ -0,0 +1,9 @@
+const navLinks = [
+  { label: "Home", href: "/#" },
+  { label: "Features", href: "/#features" },
+  { label: "Countries", href: "/#countries" },
+  { label: "Testimonials", href: "/#testimonials" },
+  { label: "Contact", href: "/#contact" },
+];
+
+export { navLinks };
--- app/ui/components/Navbar/Navbar.tsx
+++ app/ui/components/Navbar/Navbar.tsx
@@ -1,26 +1,59 @@
+"use client";
+
+import { useState } from "react";
+import Link from "next/link";
+
+import { navLinks } from "@/app/libs/constants";
+import ToggleButton from "./ToggleButton";
+
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleNavbar = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <header className="bg-gray-800 text-white">
-      <div className="container mx-auto px-6 py-4 flex justify-between items-center">
-        <div className="text-2xl font-bold">Speechify</div>
-        <nav className="space-x-4">
-          <a href="#" className="hover:text-gray-300">
-            Home
-          </a>
-          <a href="#" className="hover:text-gray-300">
-            Features
-          </a>
-          <a href="#" className="hover:text-gray-300">
-            Countries
-          </a>
-          <a href="#" className="hover:text-gray-300">
-            Testimonials
-          </a>
-          <a href="#" className="hover:text-gray-300">
-            Contact
-          </a>
-        </nav>
-      </div>
+      <nav>
+        <div className="container mx-auto px-6 py-4 flex justify-between items-center">
+          <Link href="/" className="text-2xl font-bold">
+            Speechify
+          </Link>
+
+          <div className="hidden md:block space-x-4">
+            {navLinks.map((link) => (
+              <Link
+                key={link.label}
+                href={link.href}
+                className="hover:text-gray-300"
+              >
+                {link.label}
+              </Link>
+            ))}
+          </div>
+
+          <div className="md:hidden flex items-center">
+            <ToggleButton isOpen={isOpen} onClick={toggleNavbar} />
+          </div>
+        </div>
+
+        {isOpen && (
+          <div className="md:hidden">
+            <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3 text-center">
+              {navLinks.map((link) => (
+                <Link
+                  key={link.label}
+                  href={link.href}
+                  className="hover:text-gray-300 block"
+                >
+                  {link.label}
+                </Link>
+              ))}
+            </div>
+          </div>
+        )}
+      </nav>
     </header>
   );
 };
